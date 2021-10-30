286a52601107c1748db9917a67ba564a18f34202
Viktorovich-buil <69518116+Viktorovich-buil@users.noreply.github.com>
2021-10-30 19:02:46 +0300

    Updates

--- src/components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './style.css'
-
-const Button = ({ children, styleType, ...props }) => {
-    return <button className={`button ${styleType}`} {...props}>{children}</button>;
-};
-
-export default Button;
